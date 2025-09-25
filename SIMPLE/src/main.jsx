401392587b700a634bf9fb93ce52bc69db1fa208
Enrique <enrique.barra@upm.es>
2025-09-25 12:43:33 +0200

    2025 version

--- SIMPLE/src/main.jsx
+++ SIMPLE/src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {BrowserRouter} from "react-router-dom";
-import LandingRutas2 from "./LandingRutas2.jsx";
+import {BrowserRouter} from "react-router";
+import LandingRutas from "./LandingRutas.jsx";
 
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <LandingRutas2 />
+    <LandingRutas />
   </BrowserRouter>
 )
 
